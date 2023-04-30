2100ffdbddf02808fa87b146da3d8af0186c9801
cviguers <cviguers@gmail.com>
2023-04-30 18:34:54 -0400

    thought model

---
+++ models/Thought.js
@@ -0,0 +1,79 @@
+const { Schema, Types } = require('mongoose');
+
+// define schema for reactions
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        // include virtuals and getters when converting to json
+        toJSON: {
+          virtuals: true,
+          getters: true,
+        },
+        id: false,
+      }
+);
+
+// define schema for thoughts
+const thoughtSchema = new Schema(
+    {
+        // define thoughttext field as string
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        // define createdAt field as date
+        createdAt: {
+            type: Date,
+            // default to current time
+            default: Date.now,
+            // format timestamp using dateFormat getter function
+            get: (timestamp) => dateFormat(timestamp)
+        },
+        // define username field as string
+        username: {
+            type: String,
+            // reference the User model
+            ref: "User"
+        },
+        // array of nested documents referenced through the reactionSchema
+        reactions: [reactionSchema]
+    },
+    {
+        // allow virtuals and getters to be created
+        toJSON: {
+            getters: true,
+          virtuals: true
+        },
+        id: false,
+    }
+);
+
+// create a virtual property to get reaction count associated with each thought
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+// initialize Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
