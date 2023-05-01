8a687a43bed276e37e36f5a127aa7d2a583dda6c
cviguers <cviguers@gmail.com>
2023-05-01 18:17:11 -0400

    user routes created

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,7 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
   // read/get a single user
   async getSingleUser(req, res) {
     try {
@@ -41,34 +42,32 @@ module.exports = {
   async updateUser(req, res) {
     try {
       const user = await User.findOneAndUpdate(
-        { _id: req.params.applicationId },
+        { _id: req.params.userId },
         { $set: req.body },
         { runValidators: true, new: true }
       );
 
       if (!user) {
-        return res
-          .status(404)
-          .json({ message: "No user with this id!" });
+        return res.status(404).json({ message: "No user with this id!" });
       }
 
-      res.json(application);
+      res.json(user);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
 
-    // delete a user
-    async deleteUser(req, res) {
-        try {
-          const user = await User.findOneAndDelete({ _id: req.params.userId });
-    
-          if (!user) {
-            return res.status(404).json({ message: "No user with that ID" });
-          }
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      },
+  // delete a user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
