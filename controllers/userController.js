3f7dc994061c78a92e0fdba19f33984869686357
cviguers <cviguers@gmail.com>
2023-05-01 20:48:00 -0400

    user routes complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const { User } = require("../models");
 
 module.exports = {
-  // read/get all users
+  // get - read all users
   async getUsers(req, res) {
     try {
       const users = await User.find();
@@ -11,7 +11,7 @@ module.exports = {
     }
   },
 
-  // read/get a single user
+  // get - read a single user
   async getSingleUser(req, res) {
     try {
       const user = await User.findOne({ _id: req.params.userId }).select(
@@ -28,7 +28,7 @@ module.exports = {
     }
   },
 
-  // create/post a new user
+  // post - create a new user
   async createUser(req, res) {
     try {
       const user = await User.create(req.body);
@@ -38,7 +38,7 @@ module.exports = {
     }
   },
 
-  // update/put user
+  // put - update a user
   async updateUser(req, res) {
     try {
       const user = await User.findOneAndUpdate(
@@ -58,7 +58,7 @@ module.exports = {
     }
   },
 
-  // delete a user
+  // delete - delete a user
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndDelete({ _id: req.params.userId });
@@ -66,6 +66,44 @@ module.exports = {
       if (!user) {
         return res.status(404).json({ message: "No user with that ID" });
       }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // post - add friend to user's friend list. add the entire body of the friendlist rather than the ID with the mongodb $addToSet operator
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+  // delete - remove friend from users friend list. finds the application based on ID. It then updates the friends array associated with the app in question by removing it's friendsid from the friends array.
+  async removeFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: { friendsId: req.params.friendsId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+
+      res.json(user);
     } catch (err) {
       res.status(500).json(err);
     }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
-const { getUsers, getSingleUser, createUser, updateUser, deleteUser} = require('../../controllers/userController');
+const { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend, removeFriend} = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser).put(updateUser).delete(deleteUser);
+router.route('/').get(getUsers).post(createUser).put(updateUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
