91d4f6a5992d522a3e93d6abf934940c40501ca1
cviguers <cviguers@gmail.com>
2023-05-02 16:53:04 -0400

    original crud operations for thought

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { } = require('../../controllers/thoughtController');
+
+// /api/thoughts
+
+// /api/thoughts/:thoughtsId
+
+// /api/thoughts/:thoughtId/reactions
+
+module.exports = router;
